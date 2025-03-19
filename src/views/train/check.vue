6a49f208ac9fbd6953246b28b4e4945db5565f1a
xinyu <2876917440@qq.com>
2025-03-19 17:35:40 +0800

    1.1.2 新增审核功能,修复一些bug

--- src/views/train/check.vue
+++ src/views/train/check.vue
@@ -1,13 +1,7 @@
 <template>
-  <el-steps :active="activeStep" finish-status="success">
-    <el-step title="提交申请"/>
-    <el-step title="审核中"/>
-    <el-step title="审核通过"/>
-  </el-steps>
+
 </template>
 
 <script lang="ts" setup>
-import {ref} from 'vue';
 
-const activeStep = ref(1); // 当前步骤
 </script>
